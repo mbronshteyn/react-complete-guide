7f9778aa79672c169529e854e7d5f1f253cf3979
mbronshteyn <michael.bronshteyn@gmail.com>
2020-02-24 11:21:27 -0800

    add Ref

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 
 import './Person.css'
 
 const person = (props) => {
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const inputRef = useRef(null);
+
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   return (
     <div className='Person'>
       <p onClick={props.click}>I'am {props.name} and I am {props.age} years old</p>
       <p>{props.children}</p>
-      <input type="text" onChange={props.changed} defaultValue={props.name}/>
+      <input type="text" ref={inputRef} onChange={props.changed} defaultValue={props.name}/>
     </div>
   );
 };
